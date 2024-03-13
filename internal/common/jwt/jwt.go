9f7d04e925770e20322490f80f941bc710da0b3b
Farol <farolina.rahmatunnisa@klikdokter.com>
2024-03-13 14:07:47 +0700

    fix: verify jwt

--- internal/common/jwt/jwt.go
+++ internal/common/jwt/jwt.go
@@ -16,38 +16,49 @@ var (
 	ErrUnknownClaims = errors.New("unknown claims type")
 )
 
+// Data that will be in the token
+type UserClaim struct {
+	jwt.RegisteredClaims
+	ID string
+}
+
 func Sign(ttl time.Duration, subject string) (string, error) {
 	now := time.Now()
 	expiry := now.Add(ttl)
 	t := jwt.NewWithClaims(
 		jwt.SigningMethodHS256,
-		jwt.RegisteredClaims{
-			IssuedAt:  jwt.NewNumericDate(now),
-			NotBefore: jwt.NewNumericDate(now),
-			ExpiresAt: jwt.NewNumericDate(expiry),
-			Issuer:    baseURL,
-			Audience:  jwt.ClaimStrings{baseURL},
-			Subject:   subject,
+		UserClaim{
+			RegisteredClaims: jwt.RegisteredClaims{
+				IssuedAt:  jwt.NewNumericDate(now),
+				NotBefore: jwt.NewNumericDate(now),
+				ExpiresAt: jwt.NewNumericDate(expiry),
+				Issuer:    baseURL,
+				Audience:  jwt.ClaimStrings{baseURL},
+				Subject:   subject,
+			},
+			ID: subject,
 		},
 	)
 	return t.SignedString(key)
 }
 
-func VerifyAndGetSubject(tokenString string) (string, error) {
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		// Don't forget to validate the alg is what you expect:
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-		}
-		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+func VerifyAndGetSubject(tokenString string) (*UserClaim, error) {
+	var userClaim UserClaim
+	token, err := jwt.ParseWithClaims(tokenString, &userClaim, func(token *jwt.Token) (interface{}, error) {
+		// if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		// 	return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		// }
+
 		return key, nil
 	})
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	if claims, ok := token.Claims.(jwt.RegisteredClaims); ok {
-		return claims.Subject, nil
-	} else {
-		return "", ErrUnknownClaims
+
+	// Checking token validity
+	if !token.Valid {
+		return nil, fmt.Errorf("token invalid")
 	}
+
+	return &userClaim, nil
 }
